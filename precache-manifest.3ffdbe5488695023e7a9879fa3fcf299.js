7427f15695c138b1d402ad517da924a8abadaff4
UsmanovAMRBEK <princariens@gmail.com>
2022-09-28 11:18:03 +0500

    Updates

---
+++ precache-manifest.3ffdbe5488695023e7a9879fa3fcf299.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "494988fe1c458f4e2f17",
+    "url": "/UsmanovAMRBEK/quizz-app/static/css/main.b5cb850e.chunk.css"
+  },
+  {
+    "revision": "494988fe1c458f4e2f17",
+    "url": "/UsmanovAMRBEK/quizz-app/static/js/main.494988fe.chunk.js"
+  },
+  {
+    "revision": "2759947e01ab1ea549b4",
+    "url": "/UsmanovAMRBEK/quizz-app/static/js/1.2759947e.chunk.js"
+  },
+  {
+    "revision": "485c5e1d1085e651007d",
+    "url": "/UsmanovAMRBEK/quizz-app/static/js/runtime~main.485c5e1d.js"
+  },
+  {
+    "revision": "c7bfcf5533a11a0c433008ff57deb8c1",
+    "url": "/UsmanovAMRBEK/quizz-app/index.html"
+  }
+];
